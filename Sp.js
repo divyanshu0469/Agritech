36f9044fd70e655e5e72b8c0e54f278582a46a02
divyanshu0469 <102656731+divyanshu0469@users.noreply.github.com>
2023-01-20 15:31:08 +0530

    Add files via upload

---
+++ Sp.js
@@ -0,0 +1,9 @@
+
+     function result(argument) {
+         // body...
+         var score=0;
+         if (document.getElementById('correct1').checked) {score++;}
+         if (document.getElementById('correct10').checked) {score++;}
+         alert("your score is:"+score)
+     }
+ 
